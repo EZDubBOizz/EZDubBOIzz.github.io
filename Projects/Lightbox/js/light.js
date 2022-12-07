4b73b6e5046319549738128a15a1bf2a8c9edb13
EZDubBOizz <80543754+EZDubBOizz@users.noreply.github.com>
2022-12-07 11:50:05 -0500

    gg5

--- Projects/Lightbox/js/light.js
+++ Projects/Lightbox/js/light.js
@@ -11,48 +11,55 @@ function openLightbox() {
     lightbox.src = this.src;
 }
 
-let Image1 = document.getElementById("image1");
-let Image2 = document.getElementById("image2");
-let Image3 = document.getElementById("image3");
-let Image4 = document.getElementById("image4");
-let Image5 = document.getElementById("image5");
-let Image6 = document.getElementById("image6");
-let Image7 = document.getElementById("image7");
-let Image8 = document.getElementById("image8");
-let Image9 = document.getElementById("image9");
-let Image10 = document.getElementById("image10");
-let Image11 = document.getElementById("image11");
-let Image12 = document.getElementById("image12");
-let Image13 = document.getElementById("image13");
-let Image14 = document.getElementById("image14");
-let Image15 = document.getElementById("image15");
-let Image16 = document.getElementById("image16");
-let Image17 = document.getElementById("image17");
-let Image18 = document.getElementById("image18");
+let galleryImages = document.getElementsByClassName("images");
+// let Image2 = document.getElementById("image2");
+// let Image3 = document.getElementById("image3");
+// let Image4 = document.getElementById("image4");
+// let Image5 = document.getElementById("image5");
+// let Image6 = document.getElementById("image6");
+// let Image7 = document.getElementById("image7");
+// let Image8 = document.getElementById("image8");
+// let Image9 = document.getElementById("image9");
+// let Image10 = document.getElementById("image10");
+// let Image11 = document.getElementById("image11");
+// let Image12 = document.getElementById("image12");
+// let Image13 = document.getElementById("image13");
+// let Image14 = document.getElementById("image14");
+// let Image15 = document.getElementById("image15");
+// let Image16 = document.getElementById("image16");
+// let Image17 = document.getElementById("image17");
+// let Image18 = document.getElementById("image18");
 
-Image1.onclick = openLightbox;
-Image2.onclick = openLightbox;
-Image3.onclick = openLightbox;
-Image4.onclick = openLightbox;
-Image5.onclick = openLightbox;
-Image6.onclick = openLightbox;
-Image7.onclick = openLightbox;
-Image8.onclick = openLightbox;
-Image9.onclick = openLightbox;
-Image10.onclick = openLightbox;
-Image11.onclick = openLightbox;
-Image12.onclick = openLightbox;
-Image13.onclick = openLightbox;
-Image14.onclick = openLightbox;
-Image15.onclick = openLightbox;
-Image16.onclick = openLightbox;
-Image17.onclick = openLightbox;
-Image18.onclick = openLightbox;
+for (let i = 0; i < galleryImages.length; i++) {
+    let images = galleryImages[i];
+    images.onclick = openLightbox;
+}
+// Image2.onclick = openLightbox;
+// Image3.onclick = openLightbox;
+// Image4.onclick = openLightbox;
+// Image5.onclick = openLightbox;
+// Image6.onclick = openLightbox;
+// Image7.onclick = openLightbox;
+// Image8.onclick = openLightbox;
+// Image9.onclick = openLightbox;
+// Image10.onclick = openLightbox;
+// Image11.onclick = openLightbox;
+// Image12.onclick = openLightbox;
+// Image13.onclick = openLightbox;
+// Image14.onclick = openLightbox;
+// Image15.onclick = openLightbox;
+// Image16.onclick = openLightbox;
+// Image17.onclick = openLightbox;
+// Image18.onclick = openLightbox;
 
 function closeLightbox() {
     lightboxContainer.classList.remove("display");
     lightbox.src = "";
 }
 
-lightboxCloser.onclick = closeLightbox;
-lightboxBackground.onclick = closeLightbox;
+let closers = document.getElementsByClassName("closer");
+
+for (let i = 0; i < closers.length; i++) {
+    let closer = closers[i];
+    closer.onclick = closeLightbox;
+}
